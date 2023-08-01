16b283c31ec68a2819b5170e64c5d980e924deaf
Ajit kumar <kajit0408@gmail.com>
2023-08-01 18:56:06 +0530

    variables and datatypes completed

--- variable.py
+++ variable.py
@@ -1,7 +1,19 @@
-a=1
-print (a)
-b="harry"
+a = 1
+print(a)
+b = "harry"
 print(b)
-c=100
+c = 100
 print(a+c)
-print("type of a is ",)
+d = complex(3, 3)
+e = False
+f = ["hello", "apple", [1, 2, 3], 5, 6, 7]
+g = {"name": "ajit", "age": 20, "email": "kajit0408@gmail.com"}
+print("type of a is ", type(a))
+print("type of b is ", type(b))
+print("type of c is ", type(c))
+print("type of d is ", type(d))
+print("type of e is ", type(e))
+print("type of f is ", type(f))
+print(f)
+print("type of g is ", type(g))
+print(g)
